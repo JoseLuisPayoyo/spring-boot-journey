5eb28ce8cb206de94b06e2e758318c2f853376a4
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-01 12:13:48 +0200

    creado el servicio

--- 02-backend-week2/ejercicio1/src/main/java/com/payoyo/ejercicio1/service/ProductoService.java
+++ 02-backend-week2/ejercicio1/src/main/java/com/payoyo/ejercicio1/service/ProductoService.java
@@ -1,5 +1,21 @@
 package com.payoyo.ejercicio1.service;
 
-public class ProductoService {
-    
-}
+import java.util.List;
+
+import com.payoyo.ejercicio1.dto.ProductoCreateDTO;
+import com.payoyo.ejercicio1.dto.ProductoResponseDTO;
+import com.payoyo.ejercicio1.dto.ProductoUpdateDTO;
+
+public interface ProductoService {
+
+    ProductoResponseDTO crear(ProductoCreateDTO dto);
+
+    List<ProductoResponseDTO> buscarTodos();
+
+    ProductoResponseDTO buscarPorId(Long id);
+
+    void eliminar(Long id);
+
+    ProductoResponseDTO actualizar(Long id, ProductoUpdateDTO dto);
+
+} 
---
+++ 02-backend-week2/ejercicio1/src/main/java/com/payoyo/ejercicio1/service/ProductoServiceImpl.java
@@ -0,0 +1,82 @@
+package com.payoyo.ejercicio1.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.payoyo.ejercicio1.dto.ProductoCreateDTO;
+import com.payoyo.ejercicio1.dto.ProductoResponseDTO;
+import com.payoyo.ejercicio1.dto.ProductoUpdateDTO;
+import com.payoyo.ejercicio1.entity.Producto;
+import com.payoyo.ejercicio1.exception.ProductoNoEncontradoException;
+import com.payoyo.ejercicio1.mapper.ProductoMapper;
+import com.payoyo.ejercicio1.repository.ProductoRepository;
+
+@Service
+@Transactional
+public class ProductoServiceImpl implements ProductoService{
+
+    private final ProductoRepository productoRepository;
+    private final ProductoMapper productoMapper;
+
+    public ProductoServiceImpl(ProductoRepository productoRepository, ProductoMapper productoMapper) {
+        this.productoRepository = productoRepository;
+        this.productoMapper = productoMapper;
+    }
+
+    @Override
+    public ProductoResponseDTO crear(ProductoCreateDTO dto) {
+
+        //Cambiamos a entidad
+        Producto producto = productoMapper.toEntity(dto);
+
+        //guardamos en la db
+        Producto productoGuardado = productoRepository.save(producto);
+
+        //devolvemos entidad
+        return productoMapper.toResponseDTO(productoGuardado);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ProductoResponseDTO> buscarTodos() {
+        
+        return productoRepository.findAll()
+                .stream()
+                .map(productoMapper::toResponseDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ProductoResponseDTO buscarPorId(Long id) {
+        
+        Producto producto = productoRepository.findById(id)
+            .orElseThrow(() -> new ProductoNoEncontradoException(id));
+
+        return productoMapper.toResponseDTO(producto);
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        if (!productoRepository.existsById(id)) {
+            throw new ProductoNoEncontradoException(id);
+        }
+        productoRepository.deleteById(id);
+    }
+
+    @Override
+    public ProductoResponseDTO actualizar(Long id, ProductoUpdateDTO dto) {
+        Producto productoExistente = productoRepository.findById(id)
+            .orElseThrow(() -> new ProductoNoEncontradoException(id));
+
+        productoExistente.setNombre(dto.getNombre());
+        productoExistente.setPrecio(dto.getPrecio());
+
+        Producto actualizado = productoRepository.save(productoExistente);
+        return productoMapper.toResponseDTO(actualizado);
+    }
+    
+}
