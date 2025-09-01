748659ef13394c7a06e7bd57a613aee87adc59fb
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-01 11:09:37 +0200

    entidad creada

---
+++ 02-backend-week2/ejercicio1/src/main/java/com/payoyo/ejercicio1/entity/Producto.java
@@ -0,0 +1,33 @@
+package com.payoyo.ejercicio1.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "productos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    private double precio;
+    
+}
