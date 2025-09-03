3cf43289ae070fdcae1a6b59efa292d8f51fe552
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-03 12:11:20 +0200

    creado controlador de listar todas

--- 02-backend-week2/ejercicio2/src/main/java/com/payoyo/ejercicio2/controller/ClasesController.java
+++ 02-backend-week2/ejercicio2/src/main/java/com/payoyo/ejercicio2/controller/ClasesController.java
@@ -1,8 +1,11 @@
 package com.payoyo.ejercicio2.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,4 +38,11 @@ public class ClasesController {
     }
 
     // listar todas
+    @GetMapping
+    public ResponseEntity<List<ResponseClasesDTO>> listarTodas(){
+        List<ResponseClasesDTO> clases = clasesService.listarTodas();
+
+        return ResponseEntity
+                .ok(clases);
+    }
 }
