625f96e7987bed6f93cbfe084f365302078fa2aa
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-03 11:15:27 +0200

    creado el repositorio

---
+++ 02-backend-week2/ejercicio2/src/main/java/com/payoyo/ejercicio2/repository/ClasesRepository.java
@@ -0,0 +1,7 @@
+package com.payoyo.ejercicio2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.payoyo.ejercicio2.model.Clases;
+
+public interface ClasesRepository extends JpaRepository<Clases, Long>{}
