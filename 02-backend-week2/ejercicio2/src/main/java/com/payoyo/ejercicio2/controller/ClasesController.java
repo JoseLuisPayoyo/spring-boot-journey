61627fc83e86bd7c8e41c5f6146c5b33a2cb3cf9
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-03 12:04:53 +0200

    creado el controlador de crear

---
+++ 02-backend-week2/ejercicio2/src/main/java/com/payoyo/ejercicio2/controller/ClasesController.java
@@ -0,0 +1,38 @@
+package com.payoyo.ejercicio2.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.payoyo.ejercicio2.dto.CreateClasesDTO;
+import com.payoyo.ejercicio2.dto.ResponseClasesDTO;
+import com.payoyo.ejercicio2.service.ClasesService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/clases")
+@CrossOrigin(origins = "*")
+public class ClasesController {
+    
+    private final ClasesService clasesService;
+    public ClasesController(ClasesService clasesService){
+        this.clasesService = clasesService;
+    }
+
+    // crear clase
+    @PostMapping
+    public ResponseEntity<ResponseClasesDTO> crear(@Valid @RequestBody CreateClasesDTO dto){
+        ResponseClasesDTO claseCreada = clasesService.crear(dto);
+
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(claseCreada);
+    }
+
+    // listar todas
+}
