1952ad957647e50f0ba80f30e7c2234bc05be947
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-03 12:16:56 +0200

    creado controlador para buscar por id

--- 02-backend-week2/ejercicio2/src/main/java/com/payoyo/ejercicio2/controller/ClasesController.java
+++ 02-backend-week2/ejercicio2/src/main/java/com/payoyo/ejercicio2/controller/ClasesController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,5 +45,14 @@ public class ClasesController {
 
         return ResponseEntity
                 .ok(clases);
+    }
+
+    // listar por id
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseClasesDTO> listarPorId(@PathVariable Long id){
+        ResponseClasesDTO dto = clasesService.listarPorId(id);
+
+        return ResponseEntity
+                .ok(dto);
     }
 }
