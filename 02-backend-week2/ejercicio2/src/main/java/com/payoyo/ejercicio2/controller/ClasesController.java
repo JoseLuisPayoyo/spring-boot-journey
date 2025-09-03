5dfe88a8f619bad86c79d76bb76450628e9976f3
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-03 12:21:44 +0200

    creado controlador para eliminar

--- 02-backend-week2/ejercicio2/src/main/java/com/payoyo/ejercicio2/controller/ClasesController.java
+++ 02-backend-week2/ejercicio2/src/main/java/com/payoyo/ejercicio2/controller/ClasesController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,5 +69,15 @@ public class ClasesController {
 
         return ResponseEntity
                 .ok(actualizado);
+    }
+
+    // eliminar clase
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id){
+        clasesService.eliminar(id);
+
+        return ResponseEntity
+            .noContent()
+            .build();
     }
 }
