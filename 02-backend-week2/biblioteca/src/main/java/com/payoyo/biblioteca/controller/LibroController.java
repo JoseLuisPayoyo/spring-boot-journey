5e15742b0a4d521d49d8303ed9c5c43bf6a8e6a5
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-04 19:06:30 +0200

    creado el controlador para listar y busqyeda por filtros

--- 02-backend-week2/biblioteca/src/main/java/com/payoyo/biblioteca/controller/LibroController.java
+++ 02-backend-week2/biblioteca/src/main/java/com/payoyo/biblioteca/controller/LibroController.java
@@ -1,11 +1,15 @@
 package com.payoyo.biblioteca.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.payoyo.biblioteca.dto.LibroCreateDTO;
@@ -31,5 +35,23 @@ public class LibroController {
         return ResponseEntity
             .status(HttpStatus.CREATED)
             .body(libroCeado);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<LibroResponseDTO>> listar(Pageable pageable){
+        Page<LibroResponseDTO> pagina = libroService.listar(pageable);
+
+        return ResponseEntity.ok(pagina);
+    }
+
+    @GetMapping("/buscar")
+    public ResponseEntity<Page<LibroResponseDTO>> buscar(
+        @RequestParam(required = false) String titulo,
+        @RequestParam(required = false) Long autorId,
+        Pageable pageable)
+    {
+        Page<LibroResponseDTO> resultado = libroService.buscar(titulo, autorId, pageable);
+
+        return ResponseEntity.ok(resultado);
     }
 }
