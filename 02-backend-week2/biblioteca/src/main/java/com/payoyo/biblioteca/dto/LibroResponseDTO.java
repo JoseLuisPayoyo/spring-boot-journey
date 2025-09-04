e757a472885436f2a83dfc7f93de4ea82b83fdb1
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-04 18:03:22 +0200

    creado el libro response dto

--- 02-backend-week2/biblioteca/src/main/java/com/payoyo/biblioteca/dto/LibroResponseDTO.java
+++ 02-backend-week2/biblioteca/src/main/java/com/payoyo/biblioteca/dto/LibroResponseDTO.java
@@ -1,5 +1,8 @@
 package com.payoyo.biblioteca.dto;
 
+import java.math.BigDecimal;
+
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +11,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LibroResponseDTO {
+
+    private Long id;
+    private String titulo;
+    private String isbn;
+    private BigDecimal precio;
+    private Integer anioPublicacion;
+
+    private Long autorId;
+    private String autorNombre;
     
 }
