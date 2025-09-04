cc46128eaba2aa7df904d020c9759cc87409fcaa
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-04 17:25:19 +0200

    repo para filtrar por nombre, pais o ambas

--- 02-backend-week2/biblioteca/src/main/java/com/payoyo/biblioteca/repository/AutorRepository.java
+++ 02-backend-week2/biblioteca/src/main/java/com/payoyo/biblioteca/repository/AutorRepository.java
@@ -1,7 +1,16 @@
 package com.payoyo.biblioteca.repository;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.payoyo.biblioteca.model.Autor;
 
-public interface AutorRepository extends JpaRepository<Autor, Long>{}
+public interface AutorRepository extends JpaRepository<Autor, Long>{
+
+    Page<Autor> findByNombreContainingIgnoreCase(String nombre, Pageable pageable);
+
+    Page<Autor> findByPaisIgnoreCase(String pais, Pageable pageable);
+
+    Page<Autor> findByNombreContainingIgnoreCaseAndPaisIgnoreCase(String nombre, String pais, Pageable pageable);
+}
