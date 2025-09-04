48a7bf0325221b14b1962702a760b7fd30e63d96
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-04 18:50:30 +0200

    creado el repo y servicio de buscar por filtros

--- 02-backend-week2/biblioteca/src/main/java/com/payoyo/biblioteca/repository/LibroRepository.java
+++ 02-backend-week2/biblioteca/src/main/java/com/payoyo/biblioteca/repository/LibroRepository.java
@@ -1,7 +1,16 @@
 package com.payoyo.biblioteca.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.payoyo.biblioteca.model.Libro;
 
-public interface LibroRepository extends JpaRepository<Libro, Long>{}
+public interface LibroRepository extends JpaRepository<Libro, Long>{
+
+    Page<Libro> findByTituloContainingIgnoreCase(String titulo, Pageable pageable);
+
+    Page<Libro> findByAutorId(Long autorId, Pageable pageable);
+
+    Page<Libro> findByTituloContainingIgnoreCaseAndAutorId(String titulo, Long autorId, Pageable pageble);
+}
