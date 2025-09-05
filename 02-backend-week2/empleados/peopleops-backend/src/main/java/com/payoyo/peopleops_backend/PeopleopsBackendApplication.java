572af1684a758df48c70fa8a79f470ca41a93209
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-05 10:48:25 +0200

    backend iniciado

---
+++ 02-backend-week2/empleados/peopleops-backend/src/main/java/com/payoyo/peopleops_backend/PeopleopsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.payoyo.peopleops_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleopsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleopsBackendApplication.class, args);
+	}
+
+}
---
+++ 02-backend-week2/empleados/peopleops-backend/src/test/java/com/payoyo/peopleops_backend/PeopleopsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payoyo.peopleops_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleopsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
