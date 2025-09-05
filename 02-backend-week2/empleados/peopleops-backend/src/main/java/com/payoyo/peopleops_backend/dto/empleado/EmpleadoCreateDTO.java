9d1f8960b911589d26ec077fe4d8761cbe62fda8
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-05 17:59:49 +0200

    creados los dtos de empleado

--- 02-backend-week2/empleados/peopleops-backend/src/main/java/com/payoyo/peopleops_backend/dto/empleado/EmpleadoCreateDTO.java
+++ 02-backend-week2/empleados/peopleops-backend/src/main/java/com/payoyo/peopleops_backend/dto/empleado/EmpleadoCreateDTO.java
@@ -1,5 +1,57 @@
 package com.payoyo.peopleops_backend.dto.empleado;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.payoyo.peopleops_backend.enums.EstadoEmpleado;
+import com.payoyo.peopleops_backend.enums.Puesto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class EmpleadoCreateDTO {
+
+    @NotBlank(message = "El nombre no puede ir vacio")
+    @Size(min = 2, max = 40, message = "El nombre debe tener entre 2 y 40 caracteres")
+    private String nombre;
+
+    @NotBlank(message = "El apellido no puede ir vacio")
+    @Size(min = 2, max = 60, message = "El apellido debe tener entre 2 y 60 caracteres")
+    private String apellido;
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "No es formato email")
+    private String email;
+
+    @Pattern(regexp = "^\\+?\\d{9,15}$", message = "Teléfono inválido")
+    private String telefono;
+
+    @NotNull(message = "La fecha de contratacion no puede estar vacia")
+    @PastOrPresent(message = "La fecha de contratacion es incorrecta")
+    private LocalDate fechaContratacion;
+
+    @NotNull(message = "El salario es obligatorio")
+    @DecimalMin(value = "0.00", message = "El presupuesto no puede ser negativo")
+    @Digits(integer = 12, fraction = 2, message = "El presupuesto debe tener como máximo 2 decimales")
+    private BigDecimal salario;
+
+    @NotNull(message = "El puesto es obligatorio")
+    private Puesto puesto;
+
+    @NotNull(message = "El estado del empleado es obligatorio")
+    private EstadoEmpleado estado;
+
+    @NotNull(message = "El departamento es obligatorio")
+    private Long departamentoId;
     
 }
--- 02-backend-week2/empleados/peopleops-backend/src/main/java/com/payoyo/peopleops_backend/dto/empleado/EmpleadoResponseDTO.java
+++ 02-backend-week2/empleados/peopleops-backend/src/main/java/com/payoyo/peopleops_backend/dto/empleado/EmpleadoResponseDTO.java
@@ -1,5 +1,26 @@
 package com.payoyo.peopleops_backend.dto.empleado;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.payoyo.peopleops_backend.enums.EstadoEmpleado;
+import com.payoyo.peopleops_backend.enums.Puesto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class EmpleadoResponseDTO {
     
+    private Long id;
+    private String nombre,
+                apellido,
+                email,
+                telefono, 
+                departamentoNombre;
+    private LocalDate fechaContratacion;
+    private BigDecimal salario;
+    private Puesto puesto;
+    private EstadoEmpleado estado;
 }
