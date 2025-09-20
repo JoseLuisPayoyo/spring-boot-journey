0082f0cdf37bb321564d6eaea15f3e52082a87f4
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-20 13:57:20 +0200

    creado el repo de rol y user

---
+++ 03-backed-security/biblioteca/src/main/java/com/payoyo/biblioteca/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.payoyo.biblioteca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.payoyo.biblioteca.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+}
