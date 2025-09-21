d47baf902ce0a928b4198122f051fed22cf61777
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-21 13:26:48 +0200

    datos dinamicos a la vista con thymeleaf

--- 03-backend-springMVC/web-intro/src/main/java/com/payoyo/web_intro/controllers/UserController.java
+++ 03-backend-springMVC/web-intro/src/main/java/com/payoyo/web_intro/controllers/UserController.java
@@ -1,13 +1,18 @@
 package com.payoyo.web_intro.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details() {
+    public String details(Model model) {
+        model.addAttribute("tittle", "Hola Spring");
+        model.addAttribute("name", "Jose Luis");
+        model.addAttribute("lastname", "Rodriguez");
+
         return "details";
     }
     
