786bff05940296062a665eb46781a6eca1ab5ca6
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-22 11:37:28 +0200

    listar usuarios

--- 03-backend-springMVC/web-intro/src/main/java/com/payoyo/web_intro/controllers/UserRestController.java
+++ 03-backend-springMVC/web-intro/src/main/java/com/payoyo/web_intro/controllers/UserRestController.java
@@ -1,5 +1,8 @@
 package com.payoyo.web_intro.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +37,20 @@ public class UserRestController {
         // body.put("user", user);
 
         return userDTO;
+    }
+
+    @GetMapping("/list")
+    public List<User> listar(){
+        User user1 = new User("Jose Luis", "Rodriguez");
+        User user2 = new User("Jose ", "Perez");
+        User user3 = new User("Luis", "Valenzuela");
+
+        List<User> users = Arrays.asList(user1, user2, user3);
+        // users.add(user1);
+        // users.add(user2);
+        // users.add(user3);
+
+        return users;
     }
     
 }
