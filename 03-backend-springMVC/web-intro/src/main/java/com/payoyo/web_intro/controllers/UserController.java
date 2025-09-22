cae560108ff49472bccb625d8f64829c95e5db95
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-22 11:56:21 +0200

    listar en thymeleaf

--- 03-backend-springMVC/web-intro/src/main/java/com/payoyo/web_intro/controllers/UserController.java
+++ 03-backend-springMVC/web-intro/src/main/java/com/payoyo/web_intro/controllers/UserController.java
@@ -1,7 +1,11 @@
 package com.payoyo.web_intro.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.payoyo.web_intro.models.User;
@@ -17,6 +21,16 @@ public class UserController {
         model.addAttribute("user", user);
 
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model){
+        List<User> users = new ArrayList<>();
+
+        model.addAttribute("users", users);
+        model.addAttribute("tittle", "Listado de usuarios");
+
+        return "list";
     }
     
 }
