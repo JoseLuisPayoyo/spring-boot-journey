6857176c04ea8d8de58cddf6e29539601629f98d
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-22 11:11:39 +0200

    creado el dto

--- 03-backend-springMVC/web-intro/src/main/java/com/payoyo/web_intro/controllers/UserRestController.java
+++ 03-backend-springMVC/web-intro/src/main/java/com/payoyo/web_intro/controllers/UserRestController.java
@@ -1,27 +1,39 @@
 package com.payoyo.web_intro.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.payoyo.web_intro.models.User;
+import com.payoyo.web_intro.models.dto.UserDTO;
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
+    // @GetMapping("/details")
+    // public Map<String, Object> details() {
+    //     User user = new User("Jose Luis", "Rodriguez");
+    //     Map<String, Object> body = new HashMap<>();
+
+    //     body.put("tittle", "Hola Spring");
+    //     body.put("user", user);
+
+    //     return body;
+    // }
+
     @GetMapping("/details")
-    public Map<String, Object> details() {
+    public UserDTO details() {
+        UserDTO userDTO = new UserDTO();
         User user = new User("Jose Luis", "Rodriguez");
-        Map<String, Object> body = new HashMap<>();
+        userDTO.setUser(user);
+        userDTO.setTitle("Hola desde Spring");
+        // Map<String, Object> body = new HashMap<>();
 
-        body.put("tittle", "Hola Spring");
-        body.put("user", user);
+        // body.put("tittle", "Hola Spring");
+        // body.put("user", user);
 
-        return body;
+        return userDTO;
     }
     
 }
---
+++ 03-backend-springMVC/web-intro/src/main/java/com/payoyo/web_intro/models/dto/UserDTO.java
@@ -0,0 +1,29 @@
+package com.payoyo.web_intro.models.dto;
+
+import com.payoyo.web_intro.models.User;
+
+public class UserDTO {
+
+    private String title;
+    private User user;
+
+    public UserDTO() {}
+    public UserDTO(String title, User user) {
+        this.title = title;
+        this.user = user;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    
+}
