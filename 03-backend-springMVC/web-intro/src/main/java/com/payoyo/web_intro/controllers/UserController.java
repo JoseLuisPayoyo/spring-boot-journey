9be7e93d9b3791abf30f086cee0f5954f4303f46
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-09-22 11:46:12 +0200

    mostrar email si tiene

--- 03-backend-springMVC/web-intro/src/main/java/com/payoyo/web_intro/controllers/UserController.java
+++ 03-backend-springMVC/web-intro/src/main/java/com/payoyo/web_intro/controllers/UserController.java
@@ -3,17 +3,18 @@ package com.payoyo.web_intro.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.payoyo.web_intro.models.User;
 
 @Controller
-@RequestMapping("/web")
 public class UserController {
 
     @GetMapping("/details")
     public String details(Model model) {
+        User user = new User("Jose Luis", "Rodriguez");
+
         model.addAttribute("tittle", "Hola Spring");
-        model.addAttribute("name", "Jose Luis");
-        model.addAttribute("lastname", "Rodriguez");
+        model.addAttribute("user", user);
 
         return "details";
     }
