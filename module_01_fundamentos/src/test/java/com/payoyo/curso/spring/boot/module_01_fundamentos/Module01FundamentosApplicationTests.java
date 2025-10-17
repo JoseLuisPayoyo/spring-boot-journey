8caff58307338646bcdbf99dd72a8c3400d550fb
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-10-17 17:45:27 +0200

    modulo 1. Proyecto inicializado

---
+++ module_01_fundamentos/src/test/java/com/payoyo/curso/spring/boot/module_01_fundamentos/Module01FundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payoyo.curso.spring.boot.module_01_fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module01FundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
